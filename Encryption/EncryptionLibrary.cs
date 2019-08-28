b86bbecd42a97bdab04d7133d2e25e4c04d49d4b
ismail eski <ismaile@projetgrup.com>
2019-08-28 09:27:15 +0300

    initial

---
+++ Encryption/EncryptionLibrary.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Encryption
+{
+    public static class EncryptionLibrary
+    {
+        private static byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+            var saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (var ms = new MemoryStream())
+            {
+                using (var aes = new RijndaelManaged())
+                {
+                    aes.KeySize = 256;
+                    aes.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    aes.Key = key.GetBytes(aes.KeySize / 8);
+                    aes.IV = key.GetBytes(aes.BlockSize / 8);
+
+                    aes.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        private static byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+            var saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (var ms = new MemoryStream())
+            {
+                using (var aes = new RijndaelManaged())
+                {
+                    aes.KeySize = 256;
+                    aes.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    aes.Key = key.GetBytes(aes.KeySize / 8);
+                    aes.IV = key.GetBytes(aes.BlockSize / 8);
+
+                    aes.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+
+            return decryptedBytes;
+        }
+
+        public static string EncryptText(this string input, string password = "E4lZWL@]OF2r{{)")
+        {
+            // Get the bytes of the string
+            var bytesToBeEncrypted = Encoding.UTF8.GetBytes(input);
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            // Hash the password with SHA256
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            var bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+            var result = Convert.ToBase64String(bytesEncrypted);
+
+            return result;
+        }
+
+        public static string DecryptText(this string input, string password = "E4lZWL@]OF2r{{)")
+        {
+            try
+            {
+                // Get the bytes of the string
+                var bytesToBeDecrypted = Convert.FromBase64String(input);
+                var passwordBytes = Encoding.UTF8.GetBytes(password);
+                passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+                var bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+
+                var result = Encoding.UTF8.GetString(bytesDecrypted);
+
+                return result;
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+        public static class KeyGenerator
+        {
+            public static string GetUniqueKey(int maxSize = 15)
+            {
+                var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+                var data = new byte[1];
+                using (var crypto = new RNGCryptoServiceProvider())
+                {
+                    crypto.GetNonZeroBytes(data);
+                    data = new byte[maxSize];
+                    crypto.GetNonZeroBytes(data);
+                }
+
+                var result = new StringBuilder(maxSize);
+                foreach (var b in data)
+                {
+                    result.Append(chars[b % (chars.Length)]);
+                }
+
+                return result.ToString();
+            }
+        }
+    }
+}
---
+++ Encryption/frmMain.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Encryption
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtEncrpyteText.Text))
+            {
+                MessageBox.Show("Şifrelenmiş Değer Boş Olamaz");
+                return;
+            }
+
+            try
+            {
+                if (!string.IsNullOrEmpty(txtPassword.Text))
+                {
+                    txtDecryptText.Text = txtEncrpyteText.Text.DecryptText(txtPassword.Text);
+                    return;
+                }
+                txtDecryptText.Text = txtEncrpyteText.Text.DecryptText();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void btnEncrypte_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtDecryptText.Text))
+            {
+                MessageBox.Show("Şifrelenecek Değer Boş Olamaz");
+                return;
+            }
+
+            try
+            {
+                if (!string.IsNullOrEmpty(txtPassword.Text))
+                {
+                    txtEncrpyteText.Text = txtDecryptText.Text.EncryptText(txtPassword.Text);
+                    return;
+                }
+                txtEncrpyteText.Text = txtDecryptText.Text.EncryptText();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+    }
+}
